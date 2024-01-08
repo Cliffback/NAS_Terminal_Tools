7be635dbc63469272ac59e51a502ad70cfeff523
Mathias <51080320+Cliffback@users.noreply.github.com>
2024-01-08 12:03:32 +0100

    Create lfcd.sh

---
+++ lfcd.sh
@@ -0,0 +1,16 @@
+# Use lf to switch directories ...
+lfcd () {
+    tmp="$(mktemp)"
+    lf -last-dir-path="$tmp" "$@"
+    if [ -f "$tmp" ]; then
+        dir="$(cat "$tmp")"
+        rm -f "$tmp"
+        if [ -d "$dir" ]; then
+            if [ "$dir" != "$(pwd)" ]; then
+                cd "$dir"
+            fi
+        fi
+    fi
+}
+# ... and bind it to ctrl-o
+bindkey -s '^o' 'lfcd\n'  # zsh
